a6d26aee854a18a8f20e4ee3782258b80aad3784
panini-git <60107509+panini-git@users.noreply.github.com>
2020-12-29 14:22:12 +0530

    Update SpeakHello.js

--- module-4sol/SpeakHello.js
+++ module-4sol/SpeakHello.js
@@ -1,7 +1,7 @@
 (function(window){
 var helloSpeaker={};
 var speakWord = "Hello";
-helloSpeaker.speak=function(){
+helloSpeaker.speak=function(name){
 	console.log(speakWord+" "+helloSpeaker.name);}
 	window.helloSpeaker=helloSpeaker;
 })(window);
